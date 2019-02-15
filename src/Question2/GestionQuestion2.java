b7a6ca9af5d1960c265b44992db8883ce73b1acf
Maxime Dery <maximedery@gmail.com>
2019-02-15 14:10:07 -0500

    done !

--- src/Question2/GestionQuestion2.java
+++ src/Question2/GestionQuestion2.java
@@ -1,59 +1,116 @@
 package Question2;
 
+import javax.swing.*;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class GestionQuestion2 {
+public class GestionQuestion2 implements ActionListener{
 
 	private String nom = " ";
 	private String sport = " ";
 	private String action = " ";
+	
+	private JButton bouton1;
+	private JButton bouton2;
+	private JButton bouton3;
+	private JButton bouton4;
+	private JButton bouton5;
+	private JButton bouton6;
+	private JButton bouton7;
+	private JButton bouton8;
+	private JButton bouton9;
+	private JButton bouton10;
+	private JButton bouton11;
+	private JButton bouton12;
+	private JButton bouton13;
+	private JTextField message;
+	private ButtonGroup groupNom;
+	private ButtonGroup groupAction;
+	private ButtonGroup groupSport;
 
-	public void actionPerformed(ActionEvent e) {
-
-		if (e.getSource() == Question2.groupNom) {
-			if (e.getSource() == Question2.bouton1) {
-				nom = Question2.bouton1.getText();
-			} else if (e.getSource() == Question2.bouton2) {
-				nom = Question2.bouton2.getText();
-			} else if (e.getSource() == Question2.bouton3) {
-				nom = Question2.bouton3.getText();
-			} else if (e.getSource() == Question2.bouton4) {
-				nom = Question2.bouton4.getText();
-			} else if (e.getSource() == Question2.bouton5) {
-				nom = Question2.bouton5.getText();
-			}
-		}
+	public GestionQuestion2( JButton button1, JButton button2, JButton button3, JButton button4, JButton button5,
+			JButton button6, JButton button7, JButton button8, JButton button9, JButton button10, JButton button11,
+			JButton button12, JButton button13, JTextField message, ButtonGroup groupNom, ButtonGroup groupAction, ButtonGroup groupSport ) {
+			
+		this.bouton1 = button1;
+		this.bouton2 = button2;
+		this.bouton3 = button3;
+		this.bouton4 = button4;
+		this.bouton5 = button5;
+		this.bouton6 = button6;
+		this.bouton7 = button7;
+		this.bouton8 = button8;
+		this.bouton9 = button9;
+		this.bouton10 = button10;
+		this.bouton11 = button11;
+		this.bouton12 = button12;
+		this.bouton13 = button13;
+		this.message = message;
+		this.groupAction = groupAction;
+		this.groupNom = groupNom;
+		this.groupSport = groupSport;
 		
-		if (e.getSource() == Question2.groupAction) {
-			if (e.getSource() == Question2.bouton6) {
-				action = Question2.bouton6.getText();
-			} else if (e.getSource() == Question2.bouton7) {
-				action = Question2.bouton7.getText();
-			} else if (e.getSource() == Question2.bouton8) {
-				action = Question2.bouton8.getText();
-			}
-		}
-		
-		if (e.getSource() == Question2.groupSport) {
-			if (e.getSource() == Question2.bouton9) {
-				sport = Question2.bouton9.getText();
-			} else if (e.getSource() == Question2.bouton10) {
-				sport = Question2.bouton10.getText();
-			} else if (e.getSource() == Question2.bouton11) {
-				sport = Question2.bouton11.getText();
-			}else if (e.getSource() == Question2.bouton12) {
-				sport = Question2.bouton12.getText();
-			}
-		}
-
-		if (e.getSource() == Question2.bouton13) {
-			nom = null;
-			action = null;
-			sport = null;
-			Question2.message.setText(" ");
-		}
-
-		Question2.message.setText(nom + " " + action + " " + sport);
 
 	}
+	
+	
+
+	public GestionQuestion2() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void actionPerformed( ActionEvent e ) {
+
+		
+			if ( e.getSource() == bouton1) {
+				nom = bouton1.getText();
+			} else if ( e.getSource() == bouton2 ) {
+				nom = bouton2.getText();
+			} else if ( e.getSource() == bouton3 ) {
+				nom = bouton3.getText();
+			} else if ( e.getSource() == bouton4 ) {
+				nom = bouton4.getText();
+			} else if ( e.getSource() == bouton5 ) {
+				nom = bouton5.getText();
+			}
+		
+
+		
+			if ( e.getSource() == bouton6 ) {
+				action = bouton6.getText();
+			} else if ( e.getSource() == bouton7 ) {
+				action = bouton7.getText();
+			} else if ( e.getSource() == bouton8 ) {
+				action = bouton8.getText();
+			}
+		
+
+		
+			if ( e.getSource() == bouton9 ) {
+				sport = bouton9.getText();
+			} else if ( e.getSource() == bouton10 ) {
+				sport = bouton10.getText();
+			} else if ( e.getSource() == bouton11 ) {
+				sport = bouton11.getText();
+			} else if ( e.getSource() == bouton12 ) {
+				sport = bouton12.getText();
+			}
+		
+
+		if ( e.getSource() == bouton13 ) {
+			nom = " ";
+			action = " ";
+			sport = " ";
+			message.setText( " " );
+		}
+		
+		try {
+			message.setText( nom + " " + action + " " + sport );
+		} catch ( Exception e2 ) {
+			// TODO: handle exception
+		}
+		
+
+	}
+
 }
--- src/Question2/Question2.java
+++ src/Question2/Question2.java
@@ -1,4 +1,5 @@
 package Question2;
+
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -6,75 +7,92 @@ import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
-public class Question2  extends JFrame {
-	
+public class Question2 extends JFrame {
+
 	private static final long serialVersionUID = 1L;
-	private JButton bouton1 = new JButton ("Simon");
-	private JButton bouton2 = new JButton ("Erik");
-	private JButton bouton3 = new JButton ("Margot");
-	private JButton bouton4 = new JButton ("Patrick");
-	private JButton bouton5 = new JButton ("Julie");
-	private JButton bouton6 = new JButton ("aimerait pratiquer");
-	private JButton bouton7 = new JButton ("deteste");
-	private JButton bouton8 = new JButton ("pratique");
-	private JButton bouton9 = new JButton ("l'escalade");
-	private JButton bouton10 = new JButton ("la natation");
-	private JButton bouton11 = new JButton ("le ski");
-	private JButton bouton12 = new JButton ("la raquette");
-	private JButton bouton13 = new JButton ("Nouvelle Phrase");
-	private JTextField message = new JTextField(25);
-	private JTextArea message2 = new JTextArea("Phrase:");
+	private JButton bouton1 = new JButton( "Simon" );
+	private JButton bouton2 = new JButton( "Erik" );
+	private JButton bouton3 = new JButton( "Margot" );
+	private JButton bouton4 = new JButton( "Patrick" );
+	private JButton bouton5 = new JButton( "Julie" );
+	private JButton bouton6 = new JButton( "aimerait pratiquer" );
+	private JButton bouton7 = new JButton( "deteste" );
+	private JButton bouton8 = new JButton( "pratique" );
+	private JButton bouton9 = new JButton( "l'escalade" );
+	private JButton bouton10 = new JButton( "la natation" );
+	private JButton bouton11 = new JButton( "le ski" );
+	private JButton bouton12 = new JButton( "la raquette" );
+	private JButton bouton13 = new JButton( "Nouvelle Phrase" );
+	private JTextField message = new JTextField( 25 );
+	private JTextArea message2 = new JTextArea( "Phrase:" );
 	private ButtonGroup groupNom = new ButtonGroup();
 	private ButtonGroup groupAction = new ButtonGroup();
 	private ButtonGroup groupSport = new ButtonGroup();
-	
-	public Question2(){
-		
-		super("Atelier 2 - Q2 - Maxime Dery");
+
+	public Question2() {
+
+		super( "Atelier 3 - Q2 - Maxime Dery" );
 		JPanel panneau = new JPanel();
-		setSize(400, 300);
+		setSize( 400, 300 );
 		
-		GestionQuestion2 listener1
+		
+
+		GestionQuestion2 listener1 = new GestionQuestion2( bouton1, bouton2, bouton3, bouton4, bouton4, bouton6, bouton7, bouton8, bouton9, bouton10, bouton11, bouton12, bouton13, message, groupNom, groupAction, groupSport );
+
 		groupNom.add( bouton1 );
 		groupNom.add( bouton2 );
 		groupNom.add( bouton3 );
 		groupNom.add( bouton4 );
 		groupNom.add( bouton5 );
-		panneau.add(bouton1);
-		panneau.add(bouton2);
-		panneau.add(bouton3);
-		panneau.add(bouton4);
-		panneau.add(bouton5);
-		
+		panneau.add( bouton1 );
+		panneau.add( bouton2 );
+		panneau.add( bouton3 );
+		panneau.add( bouton4 );
+		panneau.add( bouton5 );
+
 		groupAction.add( bouton6 );
 		groupAction.add( bouton7 );
 		groupAction.add( bouton8 );
-		panneau.add(bouton6);
-		panneau.add(bouton7);
-		panneau.add(bouton8);
-		
+		panneau.add( bouton6 );
+		panneau.add( bouton7 );
+		panneau.add( bouton8 );
+
 		groupSport.add( bouton9 );
 		groupSport.add( bouton10 );
 		groupSport.add( bouton11 );
 		groupSport.add( bouton12 );
-		panneau.add(bouton9);
-		panneau.add(bouton10);
-		panneau.add(bouton11);
-		panneau.add(bouton12);
-		
-		
-		panneau.add(message2);
-		panneau.add(message);
-		panneau.add(bouton13);
-		
-		add(panneau);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
+		panneau.add( bouton9 );
+		panneau.add( bouton10 );
+		panneau.add( bouton11 );
+		panneau.add( bouton12 );
+
+		panneau.add( message2 );
+		panneau.add( message );
+		panneau.add( bouton13 );
+
+		add( panneau );
+		setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+
+		bouton1.addActionListener( listener1 );
+		bouton2.addActionListener( listener1 );
+		bouton3.addActionListener( listener1 );
+		bouton4.addActionListener( listener1 );
+		bouton4.addActionListener( listener1 );
+		bouton6.addActionListener( listener1 );
+		bouton7.addActionListener( listener1 );
+		bouton8.addActionListener( listener1 );
+		bouton9.addActionListener( listener1 );
+		bouton10.addActionListener( listener1 );
+		bouton11.addActionListener( listener1 );
+		bouton12.addActionListener( listener1 );
+		bouton13.addActionListener( listener1 );
+		message.addActionListener( listener1 );
+
 	}
-	
-		
+
 	public static void main( String[] args ) {
-		Question2 fenetre= new Question2();
-		fenetre.setVisible(true);
+		Question2 fenetre = new Question2();
+		fenetre.setVisible( true );
 
 	}
 
